1e4f1541c29147699e609480d17b32d86f2792db
Juan Bejarano <jbejaranos@unal.edu.co>
2021-06-01 10:22:19 -0500

    key

--- aesci/aesci/settings.py
+++ aesci/aesci/settings.py
@@ -49,7 +49,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2=q(2xo-*=8+rultz57gclpy6gs6tvob9ayoomk65t^mdoosr('
+SECRET_KEY = os.environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
