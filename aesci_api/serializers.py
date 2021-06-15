fe5aca57f3c142bec2b68e4e8fa193a5947ef91a
solotov25 <jlugon@unal.edu.co>
2021-06-15 18:28:54 -0500

    Course model

--- aesci_api/serializers.py
+++ aesci_api/serializers.py
@@ -1,2 +1,20 @@
+from rest_framework import serializers
+from .models import Course
 
+class CourseSerializer(serializers.ModelSerializer):
+    codeCourse = serializers.IntegerField()
+    periodPlan = serializers.CharField()
+    referencePlan = serializers.IntegerField()
+    nameCourse = serializers.CharField()
+    departmentCourse = serializers.CharField()
+
+    class Meta:
+            model = Course
+            fields = '__all__'
+
+    def createCourse(self, validated_data):
+        """
+        Create and return a new `Course` instance, given the validated data.
+        """
+        return Course.objects.create(**validated_data)
 
--- aesci_api/views.py
+++ aesci_api/views.py
@@ -6,3 +6,16 @@ from rest_framework.response import Response
 from .serializers import CourseSerializer
 
 # Create your views here.
+class CourseViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    # permissions = [permissions.IsAuthenticated]
+
+    @api_view(['GET', 'POST'])
+    def hello_world(request):
+        if request.method == 'POST':
+            return Response({"message": "Got some data!", "data": request.data})
+        return Response({"message": "Hello, world!"})
