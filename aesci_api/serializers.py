170466eee361e5857293afd96a29c7d08cf56752
solotov25 <jlugon@unal.edu.co>
2021-07-16 22:08:21 -0400

    Serializers verification

--- aesci_api/serializers.py
+++ aesci_api/serializers.py
@@ -4,17 +4,6 @@ from django.contrib.auth.models import User, Group
 from .models import Course
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'groups']
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
-
-
 class CourseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Course
