87006ba1f8b86aa990fe6f4775b14b093908795a
solotov25 <jlugon@unal.edu.co>
2021-09-17 18:34:37 -0400

    Upload file in selected folder

--- aesci_api/views/UploadFile.py
+++ aesci_api/views/UploadFile.py
@@ -1,4 +1,17 @@
+import os
 from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from rest_framework.views import APIView
 
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
+drive = GoogleDrive(GoogleAuth())
+file_path = "file.txt"
+# Set up folder ID 
+studentFiles = os.environ.get('STUDENT_FOLDER')
+# Create File inside folder studentFiles
+file1 = drive.CreateFile({'parents': [{'id': studentFiles}]})
+file1.SetContentFile(file_path)
+file1.Upload()
+
+# class UploadFile(APIView):
+#     """Input file, return url to save in Drive"""
+#     def post(self, request):
