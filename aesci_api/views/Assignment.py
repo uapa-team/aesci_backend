9690cc8ded3697292e49aca03cb882f7b23e6d30
solotov25 <jlugon@unal.edu.co>
2022-01-25 18:16:02 -0500

    Query fixed

--- aesci_api/views/Assignment.py
+++ aesci_api/views/Assignment.py
@@ -10,12 +10,11 @@ class AssignmentViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows teachers to create assignments.
     """
-    queryset = Assignment.objects.all()
     serializer_class = AssignmentSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
 
-    # def get_queryset(self, pk=None):
-    #     if Teacher.objects.filter(username=self.request.query_params["username"]).exists():
-    #         # Return assignments related to Teacher
-    #         return Assignment.objects.filter(username=self.request.query_params["username"])
-    #     return Assignment.objects.all() 
+    def get_queryset(self, pk=None):
+        if Teacher.objects.filter(username=self.request.query_params["username"]).exists():
+            # Return assignments related to Teacher
+            return Assignment.objects.filter(username=self.request.query_params["username"])
+        return Assignment.objects.all() 
