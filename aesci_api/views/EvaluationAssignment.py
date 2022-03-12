50c881d091e6e1903fe3ee638590f21e44458328
Juan Bejarano <jbejaranos@unal.edu.co>
2022-03-12 13:33:06 -0500

    multiple indicators (untested)

--- aesci_api/views/EvaluationAssignment.py
+++ aesci_api/views/EvaluationAssignment.py
@@ -32,42 +32,38 @@ class EvaluationAssignmentViewSet(viewsets.ModelViewSet):
         qualifier = self.request.data["qualifier"]
         evaluationType = self.request.data["evaluationType"]
         isNumber = self.request.data["isNumber"]
-        grade = self.request.data["grade"]
-        measure = self.request.data["measure"]
         assignment = self.request.data["assignment"]
         group = self.request.data["group"]
-        indicator = self.request.data["indicator"]
+
+        indicators =''.join(request.data["indicator"])
+        indicatorsString1 = indicators.replace('[','')
+        indicatorsString2 = indicatorsString1.replace(']','')
+        indicatorsString3 = indicatorsString2.replace('"','')
+        indicatorsString4 = indicatorsString3.replace(' ','')
+        indicatorsList = list(indicatorsString4.split(","))
+
+        measures =''.join(request.data["measure"])
+        measuresString1 = measures.replace('[','')
+        measuresString2 = measuresString1.replace(']','')
+        measuresString3 = measuresString2.replace('"','')
+        measuresString4 = measuresString3.replace(' ','')
+        if measuresString4=='':
+            measuresList = list(measuresString4.split(","))
+        else:
+            measuresList = []
+
+        grades =''.join(request.data["grade"])
+        gradesString1 = grades.replace('[','')
+        gradesString2 = gradesString1.replace(']','')
+        gradesString3 = gradesString2.replace('"','')
+        gradesString4 = gradesString3.replace(' ','')
+        if gradesString4=='':
+            gradesList = list(measuresString4.split(","))
+        else:
+            gradesList = []
+
         username = self.request.data["studentUsername"]
         files = self.request.FILES.getlist('documentAttached')
-        
-
-        #Change grade into measure if the grade is a normal number
-
-        if isNumber == "True":
-
-            if float(grade) < 2.1:
-                codeMeasure = "1"
-            elif float(grade) < 3.0:
-                codeMeasure = "2"
-            elif float(grade) < 4.3:
-                codeMeasure = "3"
-            else:
-                codeMeasure = "4"
-
-        #Get the indicatorAsignment and assignmentStudent objects with info from the request
-
-        with connection.cursor() as cursor:
-            query=f'SELECT "idIndicatorAssignment" FROM aesci_api_indicatorassignment WHERE "assignment_id" = \'{assignment}\' and "indicatorGroup_id" = (SELECT "idIndicatorGroup" FROM aesci_api_indicatorgroup WHERE "numGroup_id"=\'{group}\' AND "performanceIndicator_id"=\'{indicator}\')'
-            cursor.execute(query)
-            result1=cursor.fetchone()
-            print(result1)
-            query2=f'SELECT "idAssignmentStudent" FROM aesci_api_assignmentStudent WHERE "Assignment_id" = \'{assignment}\' and "GroupStudent_id" = (SELECT "idGroupStudent" FROM aesci_api_groupstudent WHERE "numGroup_id"=\'{group}\' AND "username_id"=\'{username}\')'
-            cursor.execute(query)
-            result2=cursor.fetchone()
-            print(result2)
-
-        IndicatorAssignmentObject = IndicatorAssignment.objects.get(idIndicatorAssignment=result1[0])
-        AssignmentStudentObject = AssignmentStudent.objects.get(idAssignmentStudent=result2[0])
 
         links = []
 
@@ -101,30 +97,67 @@ class EvaluationAssignmentViewSet(viewsets.ModelViewSet):
             links.append(file1['id'])
             # Remove file from storage
             os.remove(tmp_file)
-
-
-        #Create EvaluationStudent objects
-
-        if links == []:
-
-            EvaluationAssignment.objects.create(indicatorAssignment=IndicatorAssignmentObject,
-                assignmentStudent=AssignmentStudentObject,
-                qualifier=qualifier,
-                evaluationType=evaluationType,
-                codeMeasure=codeMeasure,
-                grade=grade)
         
-        else:
-                                                
-            EvaluationAssignment.objects.create(indicatorAssignment=IndicatorAssignmentObject,
-                assignmentStudent=AssignmentStudentObject,
-                qualifier=qualifier,
-                evaluationType=evaluationType,
-                codeMeasure=codeMeasure,
-                grade=grade,
-                documentAttached= links[0])
+        for i in range(len(indicatorsList)):
 
-        return Response( status=status.HTTP_200_OK)
+            #Change grade into measure if the grade is a normal number
+
+            if isNumber == "True":
+
+                if float(gradesList[i]) < 2.1:
+                    measuresList.append("1")
+                elif float(gradesList[i]) < 3.0:
+                    measuresList.append("2")
+                elif float(gradesList[i]) < 4.3:
+                    measuresList.append("3")
+                else:
+                    measuresList.append("4")
+
+            #Get the indicatorAsignment and assignmentStudent objects with info from the request
+
+            with connection.cursor() as cursor:
+                query=f'SELECT "idIndicatorAssignment" FROM aesci_api_indicatorassignment WHERE "assignment_id" = \'{assignment}\' and "indicatorGroup_id" = (SELECT "idIndicatorGroup" FROM aesci_api_indicatorgroup WHERE "numGroup_id"=\'{group}\' AND "performanceIndicator_id"=\'{indicatorsList[i]}\')'
+                cursor.execute(query)
+                result1=cursor.fetchone()
+                print(result1)
+                query2=f'SELECT "idAssignmentStudent" FROM aesci_api_assignmentStudent WHERE "Assignment_id" = \'{assignment}\' and "GroupStudent_id" = (SELECT "idGroupStudent" FROM aesci_api_groupstudent WHERE "numGroup_id"=\'{group}\' AND "username_id"=\'{username}\')'
+                cursor.execute(query)
+                result2=cursor.fetchone()
+                print(result2)
+
+            IndicatorAssignmentObject = IndicatorAssignment.objects.get(idIndicatorAssignment=result1[0])
+            AssignmentStudentObject = AssignmentStudent.objects.get(idAssignmentStudent=result2[0])
+
+            #Create EvaluationStudent objects
+
+            if links == [] and gradesList == []:
+
+                EvaluationAssignment.objects.create(indicatorAssignment=IndicatorAssignmentObject,
+                    assignmentStudent=AssignmentStudentObject,
+                    qualifier=qualifier,
+                    evaluationType=evaluationType,
+                    codeMeasure=measuresList[i])
+
+            elif links == []:
+
+                EvaluationAssignment.objects.create(indicatorAssignment=IndicatorAssignmentObject,
+                    assignmentStudent=AssignmentStudentObject,
+                    qualifier=qualifier,
+                    evaluationType=evaluationType,
+                    codeMeasure=measuresList[i],
+                    grade=gradesList[i])
+
+            else:
+
+                EvaluationAssignment.objects.create(indicatorAssignment=IndicatorAssignmentObject,
+                    assignmentStudent=AssignmentStudentObject,
+                    qualifier=qualifier,
+                    evaluationType=evaluationType,
+                    codeMeasure=measuresList[i],
+                    grade=gradesList[i],
+                    documentAttached= links[0])
+
+        return Response("Calificación exitosa", status=status.HTTP_200_OK)
 
 	#This is not done yet
     def update(self, request, *args, **kwargs):
