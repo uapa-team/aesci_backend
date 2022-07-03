a59feb13b817cb383474b5a45e320fb072acd001
Alejandro Chávez <cchavezb@unal.edu.co>
2022-07-03 18:06:51 -0500

    BarChart returning correct results for various semesters input

--- aesci_api/views/BarChart.py
+++ aesci_api/views/BarChart.py
@@ -1,3 +1,4 @@
+from ast import Assign
 from unicodedata import numeric
 from rest_framework import status
 from rest_framework.response import Response
@@ -5,7 +6,7 @@ from rest_framework.views import APIView
 
 from django.db import connection
 
-from ..models import GroupCo, PerformanceIndicator, IndicatorGroup, IndicatorAssignment, EvaluationAssignment
+from ..models import GroupCo, PerformanceIndicator, IndicatorGroup, IndicatorAssignment, EvaluationAssignment, Assignment, AssignmentStudent, GroupStudent, Student
 
 class BarChartView(APIView):
     """Create relations between groups and students"""
@@ -120,34 +121,50 @@ class BarChartView(APIView):
                 for x in indicatorAssignmentsList:
         			#Get grades of assignments related to current performanceIndicator
                     currentIndicatorGrades = []
+                    currentIndicatorAssignments = []
                     for y in x[1]:
                         for z in y:
                             if EvaluationAssignment.objects.filter(indicatorAssignment=z.idIndicatorAssignment).exists():
-                                currentIndicatorGrades.append(EvaluationAssignment.objects.filter(indicatorAssignment=z.idIndicatorAssignment))            
+                                currentIndicatorGrades.append(EvaluationAssignment.objects.filter(indicatorAssignment=z.idIndicatorAssignment))
+                                currentIndicatorAssignments.append(z.assignment.idAssignment)
         			#We append an array with the performanceIndicator and the grades of assignments of that performanceIndicator
-                    indicatorGradesList.append([x[0],currentIndicatorGrades])
-        
-        		#Now that we got the grades of indicators let's calculate the percentages
-                print(indicatorGradesList)
-        		#Each indicator has 4 percentages corresponding to the 4 possible indicators       
-                currentIndicatorPercentages = [0,0,0,0]
-                numberEvaluations = 0
-                for x in indicatorGradesList:     
+                    indicatorGradesList.append([x[0],currentIndicatorGrades])        
+
+				#In the next array we'll save the grades per student, performande indicator and assignment
+                gradesSPA = []
+                for x in indicatorGradesList:
                     for y in x[1]:
-        				#save the number of grades of assignments related with current performanceIndicator
-                        numberEvaluations += y.count()
                         for z in y:
-        					#We'll add 1 to the index of the measure of current assignment 
-                            currentIndicatorPercentages[(int(z.codeMeasure))-1]=currentIndicatorPercentages[(int(z.codeMeasure))-1]+1
-        		#Sometimes, indicators don't have yet any assignment, so that numberEvaluations=0 then the percentage we'll be 0 as
-        		#we've defined initially in the array
-        		#If there is any assignment related with the performance indicator that has been grade then
-        		#numberEvaluations!=0
-                if numberEvaluations!=0 :
-                    currentIndicatorPercentages = [w/numberEvaluations for w in currentIndicatorPercentages]
-        		#Now add the id of the performanceIndicator along with its correspondant percentages
-                semestersStatistics.append([w, currentIndicatorPercentages])
-                print(x)
+                            assignment = z.assignmentStudent.Assignment.idAssignment
+                            groupStudent = GroupStudent.objects.get(idGroupStudent=z.assignmentStudent.GroupStudent.idGroupStudent)
+                            student = Student.objects.get(username=groupStudent.username)
+                            wasTheTupleStudentAssignmentAlreadyFound = False
+                            positionFound = 0
+                            for x in gradesSPA:
+                                if [student.username,assignment] == x[0]:
+                                    wasTheTupleStudentAssignmentAlreadyFound = True
+                                    break
+                                positionFound += 1
+                            if wasTheTupleStudentAssignmentAlreadyFound == True:
+                                gradesSPA[positionFound].append(z.codeMeasure) 
+                            else:
+                                gradesSPA.append([[student.username,assignment],z.codeMeasure])
+        		#Now, let's calculate the level of each student in their respective assignments
+                percentages = [0,0,0,0]
+                numberEvaluations = 0
+                print(gradesSPA)
+                for x in gradesSPA:     
+                    sum = 0
+                    for y in range(len(x)-1):
+        				#sum the results obtained in all performance indicator of current student in current assignment
+						#remember the tuple that tell us in which student and assignment we're currently at is x[0]
+                        sum += int(x[y+1])
+                    averageGrade = round(sum/(len(x)-1))
+        				#We'll add 1 to the index of the level of current student
+                    percentages[averageGrade-1]=percentages[averageGrade-1]+1
+                if len(gradesSPA)!=0 :
+                    percentages = [w/len(gradesSPA) for w in percentages]
+                semestersStatistics.append([w, percentages])
             return Response(semestersStatistics, status=status.HTTP_200_OK)
 
 
