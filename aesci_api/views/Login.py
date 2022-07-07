a01ac8c44667f833f5493672b4f1a18216cd6d09
Alejandro Chávez <cchavezb@unal.edu.co>
2022-07-07 01:31:14 -0500

    Added some test users to Login

--- aesci_api/views/Login.py
+++ aesci_api/views/Login.py
@@ -23,17 +23,22 @@ class LoginView(APIView):
             return Response({"Error":"Usuario o contraseña vacios"}, status=status.HTTP_401_UNAUTHORIZED)
 
         try:
-            print(username)
+            #print(username)
             user = User.objects.get(username = username)
 
         except User.DoesNotExist:
             return Response({"Error":"El usuario no existe"}, status=status.HTTP_401_UNAUTHORIZED)
-        
-        #Search LDAP's user
-        user = LDAPBackend().authenticate(request, username=username, password=password)
-        
-        if not user:
-            return Response({"Error": "Usuario no encontrado"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        testUsers = ["usertest1","usertest2","usertest3","usertest4","usertest5"]
+        if username in testUsers:
+            res = MyTokenObtainPairSerializer.get_token(user)
+            return Response( res, status=status.HTTP_200_OK)
+        else:
+            #Search LDAP's user usertest1
+            user = LDAPBackend().authenticate(request, username=username, password=password)
+            
+            if not user:
+                return Response({"Error": "Usuario no encontrado"}, status=status.HTTP_401_UNAUTHORIZED)
         
         #Create Token
         res = MyTokenObtainPairSerializer.get_token(user)
