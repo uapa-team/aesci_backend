e7d1bd46b9d1d029239199c85e3e7c280ff2f8ad
Alejandro Chávez <cchavezb@unal.edu.co>
2022-07-18 23:10:23 -0500

    IndicatorGroupBySO: Now returns if a performanceIndicator was selected when the assignment was created

--- aesci_api/views/IndicatorGroupBySO.py
+++ aesci_api/views/IndicatorGroupBySO.py
@@ -17,7 +17,7 @@ from rest_framework.views import APIView
 
 from rest_framework.renderers import JSONRenderer
 
-from ..models import Rubric, StudentOutcome, PerformanceIndicator, IndicatorMeasure, RubricStudentOutcome, Course, GroupCo, IndicatorGroup
+from ..models import IndicatorAssignment, Rubric, StudentOutcome, PerformanceIndicator, IndicatorMeasure, RubricStudentOutcome, Course, GroupCo, IndicatorGroup, IndicatorAssignment
 from ..serializers import PerformanceIndicatorSerializer, StudentOutcomeSerializer
 # Create your views here.
 
@@ -26,8 +26,6 @@ class IndicatorGroupBySOView(APIView):
         print(self.request.query_params["numgroup"])
         if self.request.query_params["purpose"] == "create":
             if IndicatorGroup.objects.filter(numGroup=self.request.query_params["numgroup"]).exists():
-                # Return assignments related to Teacher            
-                #print(IndicatorGroup.objects.all().filter(numGroup=self.request.query_params["numgroup"]))
                 IndicatorsGroupByStudentOutcome = [[]]
                 for x in IndicatorGroup.objects.all().filter(numGroup=self.request.query_params["numgroup"]):
                     serializer = StudentOutcomeSerializer(StudentOutcome.objects.get(id = x.performanceIndicator.codeSO.id))
@@ -41,4 +39,28 @@ class IndicatorGroupBySOView(APIView):
                         IndicatorsGroupByStudentOutcome.append([serializer.data])
                 return Response(IndicatorsGroupByStudentOutcome, status=status.HTTP_200_OK)
             else:
-                return None
+                return Response("No hay ningún indicator asociado a ese grupo", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        elif self.request.query_params["purpose"] == "update":
+            if IndicatorGroup.objects.filter(numGroup=self.request.query_params["numgroup"]).exists():
+                IndicatorsGroupByStudentOutcome = [[]]
+                for x in IndicatorGroup.objects.all().filter(numGroup=self.request.query_params["numgroup"]):
+                    serializer = StudentOutcomeSerializer(StudentOutcome.objects.get(id = x.performanceIndicator.codeSO.id))
+                    if serializer.data in IndicatorsGroupByStudentOutcome[0]:
+                        serializerPI = PerformanceIndicatorSerializer(PerformanceIndicator.objects.get(idPerformanceIndicator = x.performanceIndicator.idPerformanceIndicator))
+                        if IndicatorAssignment.objects.filter(indicatorGroup=x.idIndicatorGroup, assignment=self.request.query_params["assignment"]).exists():
+                            IndicatorsGroupByStudentOutcome[IndicatorsGroupByStudentOutcome[0].index(serializer.data)+1].append([serializerPI.data,"True"])
+                        else:
+                            IndicatorsGroupByStudentOutcome[IndicatorsGroupByStudentOutcome[0].index(serializer.data)+1].append([serializerPI.data,"False"])
+                    else:
+                        serializer = StudentOutcomeSerializer(StudentOutcome.objects.get(id = x.performanceIndicator.codeSO.id))
+                        IndicatorsGroupByStudentOutcome[0].append(serializer.data)
+                        serializer = PerformanceIndicatorSerializer(PerformanceIndicator.objects.get(idPerformanceIndicator = x.performanceIndicator.idPerformanceIndicator))
+                        if IndicatorAssignment.objects.filter(indicatorGroup=x.idIndicatorGroup, assignment=self.request.query_params["assignment"]).exists():
+                            IndicatorsGroupByStudentOutcome.append([serializer.data,"True"])
+                        else:
+                            IndicatorsGroupByStudentOutcome.append([serializer.data,"False"])
+                return Response(IndicatorsGroupByStudentOutcome, status=status.HTTP_200_OK)
+            else:
+                return Response("No hay ningún indicator asociado a ese grupo", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            return Response("Porfavor ingrese un valor adecuado para la variable 'purpose' ", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
